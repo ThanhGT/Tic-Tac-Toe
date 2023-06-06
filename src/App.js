1e042e5c9e400077132e8b963a423e461dfcd6aa
ThanhGT <tthanh.tan@hotmail.com>
2023-06-06 16:35:16 -0400

    added a game status that declares a winner and who goes next

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function Board() {
   function handleClick(i)
   {
     //check if the square is already filled
-    if(squares[i]) {
+    if(squares[i] | calculateWinner(squares)) {
       return;
     }
     //.slice create a copy of the squares array instead of modifying the existing array
@@ -35,8 +35,18 @@ export default function Board() {
     setXIsNext(!xIsNext); // sets xIsNext to false
   }
 
+  //determine a winner
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = "Winner: " + winner;  
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className = "board-row">
         <Square value = {squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value = {squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -54,4 +64,24 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
